c278022154292fdad8f5a9466414909b6af1f30c
Aneta Szajowska <an.szajowska@gmail.com>
2021-02-04 22:31:16 +0100

    Setup firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFirestoreDocData, useFirestore } from 'reactfire';
+
+function Burrito() {
+    const burritoRef = useFirestore()
+        .collection('tryreactfire')
+        .doc('burrito');
+
+    const burrito = useFirestoreDocData(burritoRef);
+
+    const burritoData = burrito.data;
+
+    if(!burritoData) {
+        return <p>No burrito yet fetched</p>
+    }
+    // @ts-ignore
+    return <p>The burrito is {burritoData?.yummy ? 'good' : 'bad'}</p>;
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>APP works</h1>
+            <Burrito />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FirebaseAppProvider } from 'reactfire';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuRCrDlitRA071nka9mQUI74OvC7RjwrI",
+    authDomain: "fishkey-1f7b9.firebaseapp.com",
+    projectId: "fishkey-1f7b9",
+    storageBucket: "fishkey-1f7b9.appspot.com",
+    messagingSenderId: "144061389132",
+    appId: "1:144061389132:web:9b43ca7886b66ffe5b57e9",
+    measurementId: "G-Z061NGR99V"
+};
+
+
+// @ts-ignore
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <App />
+    </FirebaseAppProvider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
