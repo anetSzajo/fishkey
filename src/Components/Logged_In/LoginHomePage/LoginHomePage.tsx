6ca7795ded1cc0f38a9d7d4d5b7225dae176c8e9
Aneta Szajowska <an.szajowska@gmail.com>
2021-02-16 10:38:59 +0100

    Implement logout component.

--- src/Components/Logged_In/LoginHomePage/LoginHomePage.tsx
+++ src/Components/Logged_In/LoginHomePage/LoginHomePage.tsx
@@ -1,14 +1,16 @@
 import {AuthCheck, ReactFireOptions, useAuth, useObservable} from 'reactfire';
 import React from "react";
-import Login from "../../Logged_Out/Login";
 import UserCard from "../UserCard/UserCard";
+import Logout from "../Logout/Logout";
+import LogoutHomePage from "../../Logged_Out/LogoutHomePgae/LogoutHomePage";
 
 export function LoginHomePage() {
     return (
         <div>
-            <AuthCheck fallback={<Login/>}>
+            <AuthCheck fallback={<LogoutHomePage/>}>
                 <h1>Welcome in your app</h1>
                 <UserCard/>
+                <Logout />
             </AuthCheck>
         </div>
     )
--- src/Components/Logged_In/Logout/Logout.tsx
+++ src/Components/Logged_In/Logout/Logout.tsx
@@ -1,9 +1,23 @@
-// export default function Logout() {
-//     const handleLogout = () => {
-//
-//     }
-//
-//     return(
-//         <button onClick={handleLogout}>Logout</button>
-//     )
-// }
+import {useAuth} from 'reactfire';
+import React from "react";
+import {useHistory} from "react-router-dom";
+
+
+export default function Logout() {
+    const auth = useAuth();
+    const history = useHistory();
+
+    const handleLogout = (event: React.MouseEvent) => {
+        event.preventDefault();
+        console.log('Logout button clicked!')
+        auth.signOut()
+                .then(r =>  console.log('Logged Out!'))
+                .catch(err => console.log('Could not logout' + err))
+        history.push('/');
+        console.log('Logged Out and history changed!')
+    }
+
+    return(
+        <button onClick={(event: React.MouseEvent<HTMLButtonElement>) => handleLogout(event)}>Logout</button>
+    )
+}
