cda3f9a82d33a6e517d6a4ff3cf488f183b664ae
Aneta Szajowska <an.szajowska@gmail.com>
2021-03-22 12:42:12 +0100

    Refactor boxes styling.

--- src/Components/Logged_In/Box/Box.tsx
+++ src/Components/Logged_In/Box/Box.tsx
@@ -8,10 +8,13 @@ type ComponentProps ={
 
 export default function Box(props: ComponentProps){
     return(
-        <div className="box">
-            <img alt="" src="/icons/box.png" className={`${props.boxClass}`}/>
-            <img alt="" src="/icons/cards.png"/>
-            <p>{props.count}</p>
+        <div className="boxWithCards">
+            <div className="box">
+                <img alt="" src="/icons/box.png" className={`${props.boxClass}`}/>
+                <div className="cards__container">{props.count}</div>
+                {/*<img alt="" src="/icons/cards.png" className="cards__container"/>*/}
+                    {/*<p>{props.count}</p>*/}
+            </div>
         </div>
     )
 }
