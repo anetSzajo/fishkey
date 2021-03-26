03884201297c5d385be43c115e36a1f5d9db6377
Aneta Szajowska <an.szajowska@gmail.com>
2021-03-26 13:46:43 +0100

    Separate the renderFlashcardTemplate function.

--- src/Components/Logged_In/Flashcard/Flashcard.tsx
+++ src/Components/Logged_In/Flashcard/Flashcard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 import {FlashcardModel} from "../../../Model/Flashcard/FlashcardModel";
 import firebase from 'firebase/app';
 import DeleteFlashcardButton from "../DeleteFlashcardButton/DeleteFlashcardButton";
@@ -76,49 +76,41 @@ export default class Flashcard extends React.Component<FlashcardModel & { NO_ID_
         })
     }
 
+    renderFlashcardTemplate = () => {
+        return(
+            <Fragment>
+                <div className="flashcard-category">{this.props.category}</div>
+                <div className="flashcard-bottom__container">
+                    <div className="flashcard-stage">{STAGES.get(this.props.stage)}</div>
+                    <div className="flashcard-btn__container">
+                        <button className="flashcard-btn correct" onClick={this.handleCorrectButton}
+                                disabled={this.props.stage === 'archived'}>
+                            <img alt="" src="/icons/correct.png"/>
+                        </button>
+                        <button className="flashcard-btn" onClick={this.handleWrongButton}>
+                            <img alt="" src="/icons/wrong.png"/>
+                        </button>
+                    </div>
+                    <button className="flashcard-rotate-btn" onClick={this.handleRotateFlashcard}>
+                        <img alt="" src="/icons/rotating-circular-arrow.png"/>
+                    </button>
+                </div>
+            </Fragment>
+        )
+    }
+
     render() {
         return (
             <div className="flashcard__container">
                 <div className={`flashcard ${this.state.isFlipped ? 'isFlipped' : null}`}>
                     <div className="flashcard__face flashcard__face--front">
                         <div className="flashcard-question">{this.props.question}</div>
-                        <div className="flashcard-category">{this.props.category}</div>
-                        <div className="flashcard-bottom__container">
-                            <div className="flashcard-stage">{STAGES.get(this.props.stage)}</div>
-                            <div className="flashcard-btn__container">
-                                <button className="flashcard-btn correct" onClick={this.handleCorrectButton}
-                                        disabled={this.props.stage === 'archived'}>
-                                    <img alt="" src="/icons/correct.png"/>
-                                </button>
-                                <button className="flashcard-btn" onClick={this.handleWrongButton}>
-                                    <img alt="" src="/icons/wrong.png"/>
-                                </button>
-                            </div>
-                            <button className="flashcard-rotate-btn" onClick={this.handleRotateFlashcard}>
-                                <img alt="" src="/icons/rotating-circular-arrow.png"/>
-                            </button>
-                        </div>
+                        {this.renderFlashcardTemplate()}
                     </div>
                     <div className="flashcard__face flashcard__face--back">
                         <div className="flashcard-answer">{this.props.answer}</div>
-                        <div className="flashcard-category">{this.props.category}</div>
-                        <div className="flashcard-bottom__container">
-                            <div className="flashcard-stage">{STAGES.get(this.props.stage)}</div>
-                            <div className="flashcard-btn__container">
-                                <button className="flashcard-btn correct" onClick={this.handleCorrectButton}
-                                        disabled={this.props.stage === 'archived'}>
-                                    <img alt="" src="/icons/correct.png"/>
-                                </button>
-                                <button className="flashcard-btn" onClick={this.handleWrongButton}>
-                                    <img alt="" src="/icons/wrong.png"/>
-                                </button>
-                            </div>
-                            <button className="flashcard-rotate-btn" onClick={this.handleRotateFlashcard}>
-                                <img alt="" src="/icons/rotating-circular-arrow.png"/>
-                            </button>
-                        </div>
+                        {this.renderFlashcardTemplate()}
                     </div>
-
                 </div>
                 {/*<DeleteFlashcardButton documentPath={`flash-${this.props.question}`}/>*/}
             </div>
